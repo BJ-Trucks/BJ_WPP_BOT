302b565d50f3277e2d953b3b44b90cd208c96b1c
Manzoor Ahmed <108570923+mr-bablu@users.noreply.github.com>
2024-03-28 02:31:56 +0500

    Add files via upload

---
+++ Gallery/lib/general.js
@@ -0,0 +1,16 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const generalmenu =`「 *Maria BOT* 」
+┌──⊰ _*🧧GENERAL🧧*_
+│⊳ 🌿 ${prefix}hi
+│⊳ 🌿 ${prefix}info
+│⊳ 🌿 ${prefix}support
+│⊳ 🌿 ${prefix}help
+│⊳ 🌿 ${prefix}runtime
+│⊳ 🌿 ${prefix}owner
+└──────────⊰
+ `
+ module.exports = generalmenu 
+ 
+ 
